cab92137e56a9ac177ccfcb10b5c96118eaa46d8
Lilia Angelova <urb.lili.an@gmail.com>
2025-01-24 17:29:00 +0100

    Update db_config.py

--- api/db_config.py
+++ api/db_config.py
@@ -4,20 +4,9 @@ from sqlalchemy.orm import sessionmaker
 
 from api.settings import settings
 
-# # Use SQLite in-memory for tests, otherwise PostgreSQL
-# DATABASE_URL = (
-#     "sqlite://"
-#     if settings.ENV == "test"
-#     else f"postgresql://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
-# )
-
 # Use SQLite in-memory for tests, otherwise PostgreSQL
 DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
-# # Database engine
-# engine = create_engine(
-#     DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
-# )
 
 # Database engine
 engine = create_engine(DATABASE_URL)
